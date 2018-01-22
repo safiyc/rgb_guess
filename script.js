88ffc730b41614bff95dfc4eb01c5119b4bc899d
Safiy Cham <csafiy@yahoo.com>
2018-01-22 09:05:03 -0800

    Add event listener to alert if clicked color is right or wrong.

--- script.js
+++ script.js
@@ -7,7 +7,23 @@ var colors = [
   "rgb(255, 0, 255)"
 ]
 var squares = document.querySelectorAll(".square");
+var pickedColor = colors[2];
+var rgbValue = document.getElementById("rgb-value");
+
+rgbValue.textContent = pickedColor;
 
 for(var i= 0; i < squares.length; i++){
+  //add colors to squares
   squares[i].style.backgroundColor = colors[i];
+  //add click listeners to squares
+  squares[i].addEventListener("click", function(){
+    //grab color of clicked square
+    var clickedColor = this.style.backgroundColor;
+    //compare color to pickedColor
+    if(clickedColor === pickedColor){
+      alert("Correct!");
+    } else {
+      alert("Wrong!");
+    }
+  });
 }
